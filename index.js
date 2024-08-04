4e1a90b3a3d05bf97b99e8d4a4c9d3e549723974
Rakesh <rakeshgandla200@gmail.com>
2024-08-04 15:43:30 +0530

    comment api added

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
-dotenv.config();
-const { db } = require('./db');
-const userRouter = require('./routers/user');
-const blogRouter = require('./routers/blog');
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")
+dotenv.config()
+const { db } = require("./db")
+const userRouter = require("./routers/user")
+const blogRouter = require("./routers/blog")
+const commentRouter = require("./routers/comment")
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use('/user', userRouter);
-app.use('/blog', blogRouter);
+app.use(cors())
+app.use(bodyParser.json())
+app.use("/user", userRouter)
+app.use("/blog", blogRouter)
+app.use("/comment", commentRouter)
 
-
-app.listen(8080, () => {
-  console.log('Server is running on port 8080');
-});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000")
+})
--- schema.js
+++ schema.js
@@ -1,36 +1,39 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require("mongoose")
+const { Schema } = mongoose
 
 const commentSchema = new Schema({
   comment: { type: String, required: true },
   date: { type: Date, default: Date },
-  user: { type: Schema.Types.ObjectId, ref: 'User' },
-  replies: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-});
+  user: { type: Schema.Types.ObjectId, ref: "User" },
+  username: { type: String },
+  replies: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  parentType: { type: String, enum: ["Comment", "Blog"], required: true },
+  parent: { type: Schema.Types.ObjectId, required: true },
+})
 
 const blogSchema = new Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  authorId: { type: Schema.Types.ObjectId, ref: 'User' },
+  authorId: { type: Schema.Types.ObjectId, ref: "User" },
   author: { type: String },
-  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+  comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   reactions: [String],
   createdAt: { type: Date },
   updatedAt: { type: Date },
   media: [String],
-});
+})
 
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   profilePicture: { type: String },
   username: { type: String, required: true, unique: true },
-  blogs: [{ type: Schema.Types.ObjectId, ref: 'Blog' }],
-  following: [{ type: Schema.Types.ObjectId, ref: 'User' }],
-});
+  blogs: [{ type: Schema.Types.ObjectId, ref: "Blog" }],
+  following: [{ type: Schema.Types.ObjectId, ref: "User" }],
+})
 
-const Blog = mongoose.model('Blog', blogSchema);
-const User = mongoose.model('User', userSchema);
-const Comment = mongoose.model('Comment', commentSchema);
+const Blog = mongoose.model("Blog", blogSchema)
+const User = mongoose.model("User", userSchema)
+const Comment = mongoose.model("Comment", commentSchema)
 
-module.exports = { Comment, Blog, User };
+module.exports = { Comment, Blog, User }
